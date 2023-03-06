10a37352d156c7f9420e136850a8426560af44b6
rosabranca <126729422+rosabranca@users.noreply.github.com>
2023-03-06 19:24:45 -0300

    Arrumando divs das propostas

--- script.js
+++ script.js
@@ -12,3 +12,67 @@ function nextImage() {
   }
   document.getElementById('radio' + count).checked = true
 }
+
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
+function verMaisEsportes() {
+  if (document.getElementById('ulEsportes').style.display == 'block') {
+    document.getElementById('ulEsportes').style.display = 'none'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver mais'
+  } else {
+    document.getElementById('ulEsportes').style.display = 'block'
+    document.getElementById('verMaisEsportes').innerHTML = 'Ver menos'
+  }
+}
